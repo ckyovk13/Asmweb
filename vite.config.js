bc6d7873efdee46ad765bdb0ffa8456dc232ed48
ckyovk13 <tainhph26796@fpt.edu.vn>
2022-06-10 10:25:08 +0700

    Update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   build: {
     rollupOptions: {
       input: {
-        home: resolve(__dirname, 'home.html'),
+        index: resolve(__dirname, 'index.html'),
         Blog: resolve(__dirname, 'Blog.html'),
         Works: resolve(__dirname, 'Works.html'),
         Detail: resolve(__dirname, 'detail.html'),
